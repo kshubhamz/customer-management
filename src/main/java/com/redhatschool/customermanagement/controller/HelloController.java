387d010f5f469132fa68cd7c1ec8aa4818043411
Shubham Kumar <shubhamkumar@Shubhams-MacBook-Air.local>
2022-07-11 15:32:36 +0530

    added root route

--- src/main/java/com/redhatschool/customermanagement/controller/HelloController.java
+++ src/main/java/com/redhatschool/customermanagement/controller/HelloController.java
@@ -4,18 +4,23 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class HelloController {
 	
 	@Value("${controller.hello.name.default}")
 	private String defaultName;
 	
 	@GetMapping
+	public Map<String, String> greet() {
+		return Map.of("name", "customer-management",
+				"version", "0.0.1",
+				"description", "A microservice component build to manage customer entity as a part of an assignment");
+	}
+	
+	@GetMapping("/hello")
 	public Map<String, String> getName(@RequestParam(required = false) String name) {
 		if (name == null)
 			return Map.of("name", defaultName);
